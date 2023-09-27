b4b0ba63375f7e4dd2aadfec568a5553cbb79de3
adrianj <ajperez@hitpromo.net>
2023-09-27 00:40:07 -0400

    application with default configurations

---
+++ src/main/java/com/springusers/rest/webservices/usersweb/services/UsersWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.springusers.rest.webservices.usersweb.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springusers/rest/webservices/usersweb/services/UsersWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springusers.rest.webservices.usersweb.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
